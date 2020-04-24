2eb132b729d8d503d8abc203fa0a5d4889723451
Christian Melendez <christianhxc@gmail.com>
2020-04-24 10:19:12 +0200

    Added the kubernetes provider for Terraform

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,8 +1,17 @@
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.9"
+}
+
 module "eks" {
-  source       = "terraform-aws-modules/eks/aws"
-  cluster_name = local.cluster_name
-  vpc_id       = module.vpc.vpc_id
-  subnets      = module.vpc.private_subnets
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = local.cluster_name
+  cluster_version = "1.15"
+  vpc_id          = module.vpc.vpc_id
+  subnets         = module.vpc.private_subnets
 
   worker_groups = [
     {
